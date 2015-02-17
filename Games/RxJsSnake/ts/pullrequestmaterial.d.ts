d59f8a30831833c93274dcf1b1f5e30474f03fa0
Herman Banken <hermanbanken@gmail.com>
2015-02-17 20:20:43 +0100

    Some not more interfaces not in DefinitelyTyped

--- Games/RxJsSnake/ts/pullrequestmaterial.d.ts
+++ Games/RxJsSnake/ts/pullrequestmaterial.d.ts
@@ -4,5 +4,8 @@ declare module Rx {
     
     export interface Observable<T> {		
 		withLatestFrom<T2, TResult>(second: Observable<T2>, resultSelector: (v1: T, v2: T2) => TResult): Observable<TResult>;
+		retryWhen<T2>(notifier: (errors: Observable<Error>) => Observable<T2>): Observable<T>;
+	
+//		flatScan<TResult>(seed: TResult, resultSelector: (value: T, acc: TResult) => Observable<TResult>): Observable<TResult>;
 	}
 }
