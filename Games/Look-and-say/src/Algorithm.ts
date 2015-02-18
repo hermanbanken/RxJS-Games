5e69d7ab3a47e5b07f9acc650ce44b706841b74f
Herman Banken <hermanbanken@gmail.com>
2015-02-18 15:59:13 +0100

    Fix Look and Say sequence

--- Games/Look-and-say/src/Algorithm.ts
+++ Games/Look-and-say/src/Algorithm.ts
@@ -9,7 +9,8 @@ class State {
 }
 
 export function iteration(source: Rx.Observable<number>){
-	var it = source.concat(Rx.Observable.just(-1)).scan<State[]>([], (state, value) => {
+	return source.concat(Rx.Observable.just(-1))
+	.scan<State[]>([], (state, value) => {
 		if(state.length === 0){
 			return [new State(value, 1)];
 		} else if((state[0]).value === value) {
@@ -17,27 +18,25 @@ export function iteration(source: Rx.Observable<number>){
 		} else {
 			return [new State(value, 1), state[0]];
 		}
-	}).filter(s => s.length > 1).flatMap(s => Rx.Observable.of(s[1].count, s[1].value));
-	return it;
+	}).filter(s => s.length > 1)
+	.flatMap(s => Rx.Observable.just(s[1].count).concat(Rx.Observable.just(s[1].value)));
 }
 
 export function run() {
-	return Rx.Observable.interval(1000).scan(
+	return Rx.Observable.interval(100).scan(
 		Rx.Observable.just(1),
-		(seq, _) => iteration(seq)
+		(seq, _) => iteration(seq).zip(Rx.Observable.interval(100), (a,_) => a)
 	).startWith(Rx.Observable.just(1));
 }
 
-run().take(10).subscribe(seq => seq.toArray().subscribe(list => console.log(list)));
+run().take(40).subscribe(seq => {
+	var i = 0, list = $("<li></li>");
+	list.appendTo($("#lookandsay"));
+	seq.subscribe(n => { list.append($("<span>"+(i !== 0 ? ", " : "") + n+"</span>")); i++; });
+});
 
-iteration(Rx.Observable.fromArray([1, 1, 1, 3, 2, 1, 3, 2, 1, 1])).toArray().subscribe(list => {
-	console.log("Should be equal", list, [3, 1, 1, 3, 1, 2, 1, 1, 1, 3, 1, 2, 2, 1])
-})
-
-iteration(iteration(iteration(iteration(iteration(iteration(iteration(iteration(Rx.Observable.just(1))))))))).toArray().subscribe(l => console.log("Was", l));
-
-function id<A>(a: A): A {
+function id<A>(a: A): A { 
 	return a;
 }
-// var LookAndSay = Rx.Observable.just(1).flatMap()
+
 }
