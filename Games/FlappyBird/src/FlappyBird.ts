e3fbecfb2f903726727de743c9d6d18162271dc7
Herman Banken <hermanbanken@gmail.com>
2015-04-21 17:38:00 +0200

    Fix points; add EWI; add Aula; add Clouds; add EWI boost

--- Games/FlappyBird/src/FlappyBird.ts
+++ Games/FlappyBird/src/FlappyBird.ts
@@ -9,7 +9,10 @@ module Flappy {
 	var colors = ["#629E60", "#E9C03A", "#B74133", "#5374ED"];
 	var graphics = {
 		trees: ["FlappyBird/tree.png", "FlappyBird/tree-high.png", "FlappyBird/Junglewood_Tree.png", "FlappyBird/Pearlwood_Tree.png"],
+		clouds: ["FlappyBird/cloud.png"],
 		flappy: "FlappyBird/sprites.png",
+		aula: "FlappyBird/aula.png",
+		ewi: "FlappyBird/ewi-blur.png",
 		coin: "FlappyBird/spinning_coin_gold.png"
 	};
 	var images = {};
@@ -17,7 +20,9 @@ module Flappy {
 	var flapSpeed = 300;
 	var gravity = 500;
 	var roll = Math.PI/100;
+	var canvas_height = 0;
 	var speed = 100;
+	var default_speed = 100;
 	var debug = false;
 
 	interface Stage {
@@ -42,6 +47,8 @@ module Flappy {
 
 	class SheetSprite implements Sprite {
 		f:number = 0;
+		frame_offset_x = 0;
+		frame_offset_y = 0;
 		constructor(
 			public sheet: HTMLImageElement, 
 			public sheet_offset: math.Point2D, 
@@ -61,8 +68,8 @@ module Flappy {
 				this.sheet_offset.y,
 				this.frame_width,
 				this.frame_height,
-				0,
-				0,
+				this.frame_offset_x,
+				this.frame_offset_y,
 				this.frame_width,
 				this.frame_height
 			);
@@ -115,20 +122,24 @@ module Flappy {
 		x: number;
 		width: number;
 		height: number;
+		fatal: boolean;
 	}
 
 	class BasicObstacle extends SheetSprite implements Obstacle {
 		public x = 400;
 		public width : number;
 		public height : number;
+		public y = 0;
+		public fatal = true;
 		
 		boost() { return 0; }
 		draw(ctx: CanvasRenderingContext2D) {
-			if (debug) this.box().draw(ctx, debug);
+			var b = this.box();
+			if (debug) b.draw(ctx, debug);
 			if (this.x - this.width / 2 > ctx.canvas.width || this.x + this.width / 2 < 0)
 				return;
 			ctx.save();
-			ctx.translate(this.x - this.frame_width / 2, ctx.canvas.height - this.height);
+			ctx.translate(this.x - this.frame_width / 2, ctx.canvas.height - b.centre.y - this.height / 2);
 			super.draw(ctx);
 			ctx.restore();
 		}
@@ -137,7 +148,7 @@ module Flappy {
 			return this;
 		}
 		box() {
-			return new math.Box(new math.Point2D(this.x, this.height / 2), this.width, this.height);
+			return new math.Box(new math.Point2D(this.x, this.height / 2 + this.y), this.width, this.height);
 		}
 		lines() { return this.box().lines(); }
 		delta(deltaT: number) {
@@ -151,13 +162,13 @@ module Flappy {
 		public x = 400;
 		public width = 32;
 		public height = 32;
+		public y = 20;
 		constructor() {
 			super(images[graphics.coin], new math.Point2D(0, 0), 32, 32, 8);
 		}
 	}
 
 	class StaticObstacle extends BasicObstacle {
-		public x = 400;
 		public width = 60;
 		constructor(public height: number){
 			super(null, new math.Point2D(0, 0), 0, 0, 1);
@@ -167,13 +178,47 @@ module Flappy {
 		}
 	}
 
-	class EWI extends StaticObstacle {
-		public x = 400;
+	class Aula extends BasicObstacle {
+		constructor() {
+			super(images[graphics.aula], new math.Point2D(0, 0), 400, 300, 1);
+			this.width = this.frame_width = this.sheet.width;
+			this.height = 128;
+			this.frame_height = this.sheet.height;
+		}
+		box() {
+			return new math.Box(new math.Point2D(this.x, 236), this.width, 128);
+		}
+	}
+
+	class TopDownObstacle extends BasicObstacle {
 		public width = 60;
-		height: number = 0;
-		constructor(canvas_height: number){
-			super(canvas_height);
-			this.height = canvas_height;
+		constructor(public height: number) {
+			super(null, new math.Point2D(0, 0), 0, 0, 1);
+			this.sheet = images[graphics.trees[~~(Math.random() * graphics.trees.length)]];
+			this.width = this.frame_width = this.sheet.width;
+			this.height = this.frame_height = this.sheet.height;
+			this.y = canvas_height - this.height;
+		}
+	}
+
+	class Cloud extends BasicObstacle {
+		constructor() {
+			super(null, new math.Point2D(0, 0), 0, 0, 1);
+			this.sheet = images[graphics.clouds[~~(Math.random() * graphics.clouds.length)]];
+			this.width = this.frame_width = this.sheet.width;
+			this.height = this.frame_height = this.sheet.height;
+			this.y = canvas_height - this.height - Math.random() * 200;
+		}
+	}
+
+	class EWI extends BasicObstacle {
+		public speed = 200;
+		public fatal = false;
+		constructor(){
+			super(images[graphics.ewi], new math.Point2D(0,0), 300, 500, 1);
+			this.y = 0;
+			this.width = this.frame_width;
+			this.height = this.frame_height;
 		}
 		boost() { return 100; }
 	}
@@ -183,11 +228,13 @@ module Flappy {
 
 		constructor(public points: number, public flappy: Flappy, public obstacles: Obstacle[], public coins: Coin[]){
 			if(this.obstacles.length == 0){
+				this.obstacles.push(new EWI().delta(-4));
+				this.obstacles.push(new Aula().delta(-8));
 				this.obstacles.push(new StaticObstacle(100).delta(-1));
-				this.obstacles.push(new StaticObstacle(200).delta(-3));
-				this.obstacles.push(new StaticObstacle(250).delta(-5.5));
-				this.obstacles.push(new StaticObstacle(300).delta(-7));
-				this.obstacles.push(new StaticObstacle(200).delta(-9));
+				this.obstacles.push(new Cloud().delta(-3));
+				this.obstacles.push(new Cloud().delta(-5.5));
+				this.obstacles.push(new StaticObstacle(300).delta(-11));
+				this.obstacles.push(new Cloud().delta(-14));
 				this.obstacles.push(new StaticObstacle(100).delta(-11));
 				this.obstacles.push(new StaticObstacle(300).delta(-13));
 			}
@@ -215,33 +262,43 @@ module Flappy {
 			}, false), false);
 		}
 
+		static flappyXline = new math.Line2D(new math.Point2D(Flappy.x, -10), new math.Point2D(Flappy.x, canvas_height + 10));
+		
+		/* Handles game logic per frame */
 		delta(deltaT: number, flap: boolean){
 			var f = this.flappy.delta(deltaT);
 			if(flap)
 				f = f.flap();
 
+			// Collisions
 			var fl = this.flappy.lines();
-			var collision = this.obstacles.reduce((p, o) => p || NormalStage.anyIntersection(o.lines(), fl), false);
+			var collision = this.obstacles.reduce((p, o) => p || o.fatal && NormalStage.anyIntersection(o.lines(), fl), false);
 			if (collision){
 				console.log("Collision!", collision);
 				throw new Error("Dead by collision!");
 			}
 
-			var win = 0;
+			// Boosting
+			var boost = this.obstacles.reduce((p, o) => p + (!o.fatal && NormalStage.anyIntersection(o.lines(), [NormalStage.flappyXline]) && o.boost() || 0), 0);
+			if (boost){
+				speed = default_speed + boost;
+			} else {
+				speed = default_speed;
+			}
 
+			var win = 0;
 			// Coins
 			var cs_all = this.coins.map(c => c.delta(deltaT));
-			try {
-				var cs = this.coins.filter(c => !NormalStage.anyIntersection(c.lines(), fl));
-				win += (cs_all.length - cs.length) * 1000;
-			}catch(e){
-				debugger;
-			}
+			var cs = this.coins.filter(c => !NormalStage.anyIntersection(c.lines(), fl));
+			win += (cs_all.length - cs.length) * 1000;
+			
 			// Move obstacles
 			var os = this.obstacles.map((o:Obstacle) => {
+				var ox = o.x;
 				var n = o.delta(deltaT);
-				if (n.x < Flappy.x && o.x >= Flappy.x)
+				if (n.x < Flappy.x && ox >= Flappy.x){
 					win += o.height;
+				}
 				return n;
 			});
 
@@ -252,7 +309,7 @@ module Flappy {
 				var last = this.obstacles[this.obstacles.length - 1];
 				var x = Math.max(last.x + last.width + 100, 400) + Math.random() * 200;
 				if (Math.random() > 0.8)
-					os.push(new EWI(500).at(x));
+					os.push(new Cloud().at(x));
 				else
 					os.push(new StaticObstacle(300 * Math.random() + 100).at(x));
 			}
@@ -317,14 +374,18 @@ module Flappy {
 		constructor(public ctx: CanvasRenderingContext2D) {
 			var imageLoaded = Rx.Observable.merge(
 				Rx.Observable.from(graphics.trees).flatMap(t => new Img(t).observable.take(1)),
+				Rx.Observable.from(graphics.clouds).flatMap(t => new Img(t).observable.take(1)),
 				new Img(graphics.coin).observable.take(1),
-				new Img(graphics.flappy).observable.take(1)
+				new Img(graphics.flappy).observable.take(1),
+				new Img(graphics.ewi).observable.take(1),
+				new Img(graphics.aula).observable.take(1)
 			).scan({}, (registry, img) => {
 				registry[img.src] = img.img; 
 				return registry;	
 			}).last().tap(registry => {
 				images = registry;
 			});
+			canvas_height = ctx.canvas.height;
 
 			imageLoaded.select(_ => new Start())
 			.flatScan<Stage,Stage>(
